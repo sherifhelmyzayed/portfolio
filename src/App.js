3cfa02cc3b53a9df478efe00a4ba268a3499c48f
sherif zayed <sherifhelmyzayed@gmail.com>
2022-11-26 14:20:54 +0200

    Added empty room JS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, Suspense, useState, createContext } from "react";
+import { Canvas, extend } from "@react-three/fiber";
+import { OrbitControls, ContactShadows, useProgress, Html } from "@react-three/drei";
+// import { Model } from "./Models/Apartments.js";
 
-function App() {
+export const HandlerContext = createContext()
+
+
+
+extend({ OrbitControls });
+
+
+
+const Loader = () => {
+  const { total } = useProgress()
+
+  let cal = total / 30 * 100;
+  console.log(cal);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Html center>loading.. {Math.round(cal)} %</Html>
+  )
+};
+
+
+
+export default function App() {
+
+  const [selectedApt, setSelectedApt] = useState(null);
+
+
+  const controls = useRef(null);
+
+
+
+  return (
+    <>
+
+      <Canvas
+        camera={{ fov: 55, zoom: 1, near: 200, far: 200000, position: [0, 400, 4000], }} style={{
+          // height: `100vh`, width: '100vw' 
+          height: '720px', width: '1200px'
+        }} >
+        <fog attach="fog" args={['#17171b', 0, 100000]} />
+        <color attach="background" args={['#ffffff']} />
+
+        <OrbitControls
+          enablePan={false}
+          enableZoom={true}
+          enableRotate={true}
+          autoRotate={false}
+          enableDamping={false}
+          autoRotateSpeed={1}
+          zoomSpeed={0.7}
+          minDistance={0}
+          maxDistance={10000}
+          ref={controls}
+          maxPolarAngle={1.73}
+        />
+
+        <hemisphereLight color="#00000" groundColor="#000000" position={[-7, 15, 5]} intensity={1} />
+        <pointLight position={[1000, 1000, 1000]} intensity={.5} />
+
+        <Suspense fallback={<Loader />}>
+          {/* <Model controls={controls} selectedApt={selectedApt} setSelectedApt={setSelectedApt}
+          /> */}
+        </Suspense>
+
+        {/* <ContactShadows frames={1} position={[0, -520, 0]} scale={10000} blur={1} far={9000} /> */}
+      </Canvas>
+    </>
   );
 }
-
-export default App;
