e8ecfa9cdb5528e4b47978de30e256726d3ba3dc
sherif zayed <sherifhelmyzayed@gmail.com>
2022-11-30 06:57:30 +0200

    feat: added about content

--- src/Components/AboutModal.js
+++ src/Components/AboutModal.js
@@ -8,13 +8,20 @@ const AboutModal = (props) => {
             aria-labelledby="modal-modal-title"
             aria-describedby="modal-modal-description"
             onClose={() => props.setShowAbout(false)}
+            sx={{height: '100vh'}}
         >
-            <Box >
+            <Box>
                 <Typography id="modal-modal-title" variant="h6" component="h2">
-                    Text in a modal
+                    Welcome to my virtual room
                 </Typography>
                 <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                    Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                    I understand that a porfolio is the most important tool to show you who am I, what I am capable of, and it helps you know my goals with great passion
+                </Typography>
+                <Typography>
+                    Hence, My story in this virtual room. You are granted to go through my stuff!!
+                </Typography>
+                <Typography>
+                    This project is developed in Reactm React-Three-fiber, Drei, MUI
                 </Typography>
             </Box>
         </Modal>)
